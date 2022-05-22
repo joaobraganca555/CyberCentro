1d80b1ee8de77007c65d8d4e67650a99aada1fd8
JOÃO MIGUEL TEIXEIRA BRAGANÇA <8190555@estg.ipp.pt>
2022-05-22 16:26:54 +0100

    login page

--- CyberApp/src/app/app-routing.module.ts
+++ CyberApp/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { StructureComponent } from './shared/structure/structure.component';
 
 const routes: Routes = [
+  { path: '', component: LoginComponent},
+  { path: 'menu', component: StructureComponent}
 ];
 
 @NgModule({
