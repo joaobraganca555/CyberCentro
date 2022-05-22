519787e23811cf89608a269fef4485faa49e18f2
JOÃO MIGUEL TEIXEIRA BRAGANÇA <8190555@estg.ipp.pt>
2022-05-22 22:06:33 +0100

    starting login

--- CyberApp/src/app/app.module.ts
+++ CyberApp/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
@@ -9,6 +8,7 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { MainComponent } from './shared/main/main.component';
 import { LoginComponent } from './login/login.component';
 import { StructureComponent } from './shared/structure/structure.component';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -22,9 +22,10 @@ import { StructureComponent } from './shared/structure/structure.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [FormBuilder],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- CyberApp/src/app/login/login.component.ts
+++ CyberApp/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,10 +8,19 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form = this.fb.group({
+    email: '',
+    password: ''
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+  }
+
+  submit(): void {
+    console.log(this.form.getRawValue());
+    
   }
 
 
