e6670d5778418de399a79b332edbe60373ef01ed
JOÃO MIGUEL TEIXEIRA BRAGANÇA <8190555@estg.ipp.pt>
2022-05-27 13:46:28 +0100

    width chart + icons

--- CyberApp/src/app/shared/structure/structure.component.ts
+++ CyberApp/src/app/shared/structure/structure.component.ts
@@ -7,23 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StructureComponent implements OnInit {
 
-  public data: any[];
-
-  constructor() {
-    this.data = [
-      { Year: "2009", Europe: 31 },
-      { Year: "2010", Europe: 43 },
-      { Year: "2011", Europe: 66 },
-      { Year: "2012", Europe: 69 },
-      { Year: "2013", Europe: 58 },
-      { Year: "2014", Europe: 40 },
-      { Year: "2015", Europe: 78 },
-      { Year: "2016", Europe: 13 },
-      { Year: "2017", Europe: 78 },
-      { Year: "2018", Europe: 40 },
-      { Year: "2019", Europe: 80 },
-    ];
-  }
+  constructor() {}
 
   ngOnInit(): void {
   }
